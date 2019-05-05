4607e612c8ff2c257188c95e9c524a47f41b8042
caisr <383911973@qq.com>
2019-05-05 16:43:55 +0800

    增加givelike组件的测试用例

--- src/components/give-like/give-like.js
+++ src/components/give-like/give-like.js
@@ -1,4 +1,7 @@
 Component({
+  options: {
+    multipleSlots: true
+  },
   properties: {
     defaultActive: {
       type: Boolean,
--- test/components/give-like.test.js
+++ test/components/give-like.test.js
@@ -5,29 +5,25 @@ const giveLike = simulate.load(setCompontentPath('give-like/give-like'))
 describe('give-like', () => {
   it('render', () => {
     const component = simulate.render(giveLike)
-    expect(component.dom.innerHTML).toContain('<wx-view class="ac-iconfont icon-like"></wx-view>')
+    expect(component.dom.innerHTML).toContain('<wx-view class="ac-iconfont icon-like" style=""></wx-view>')
   })
   it('give-like组件 支持默认激活', () => {
-    const root = simulate.load({
-      template: '<ac-give-like defaultActive="{{true}}"></ac-give-like>',
-      usingComponents: {
-        'ac-give-like': giveLike
-      }
-    })
-    const component = simulate.render(root)
+    const component = simulate.render(giveLike, {defaultActive: true})
     const childComp = component.dom.querySelector('.icon-like-active')
     expect(childComp.classList).toContain('visible')
   })
   it('give-like组件 支持自定义提示文字', () => {
-    const root = simulate.load({
-      template: '<ac-give-like tips="赞"></ac-give-like>',
-      usingComponents: {
-        'ac-give-like': giveLike
-      }
+    const component = simulate.render(giveLike, {tips: '赞'})
+    const tipsElem = component.querySelector('.give-like-text')
+    expect(tipsElem.dom.innerHTML).toEqual('赞')
+  })
+  it('give-like组件 支持改变按钮尺寸', () => {
+    const component = simulate.render(giveLike, {fontSize: 100})
+    let icons = component.querySelectorAll('.ac-iconfont')
+    icons = Array.from(icons)
+    icons.forEach(elem => {
+      expect(elem.dom.style._values['font-size']).toEqual('100px')
     })
-    const component = simulate.render(root)
-    const childComp = component.dom.querySelector('.give-like-text')
-    expect(childComp.innerHTML).toEqual('赞')
   })
   it('give-like组件 可以响应onclick事件', async () => {
     const component = simulate.render(giveLike)
