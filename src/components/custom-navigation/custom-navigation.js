fa98e26c0dce0dc00c69235c63f8b068123c4171
caisr <383911973@qq.com>
2019-05-10 18:21:35 +0800

    完成自定义导航栏组件

---
+++ src/components/custom-navigation/custom-navigation.js
@@ -0,0 +1,90 @@
+Component({
+  options: {
+    multipleSlots: true
+  },
+  externalClasses: ['custom-class-title', 'custom-class-img'],
+  properties: {
+    title: {
+      type: String,
+      value: ''
+    },
+    navBarTextColor: {
+      type: String,
+      value: 'black'
+    },
+    navBarIconColor: {
+      type: String,
+      value: ''
+    },
+    pillPaddingBottom: {
+      type: Number,
+      value: 5
+    },
+    customLeftCorner: {
+      type: Boolean,
+      value: false
+    },
+    homePath: {
+      type: String,
+      value: ''
+    },
+    backgroundColor: {
+      type: String,
+      value: 'transparent'
+    },
+    zIndex: {
+      type: String,
+      optionalTypes: [Number],
+      value: 3
+    },
+    bgImage: {
+      type: String,
+      value: ''
+    },
+    imgMode: {
+      type: String,
+      value: 'widthFix'
+    }
+  },
+  lifetimes: {
+    attached() {
+      this.initStyle()
+      this.setData({
+        visibleLeftCornerBtns: getCurrentPages().length > 1
+      })
+    },
+    ready() {
+      this.triggerEvent('onlayout', {height: this.data.height})
+    }
+  },
+  data: {
+    height: 0,
+    left: 0
+  },
+  methods: {
+    initStyle() {
+      const {
+        bottom, right,
+        height, top, width
+      } = wx.getMenuButtonBoundingClientRect()
+      const {windowWidth} = wx.getSystemInfoSync()
+      const totalHeight = bottom + this.data.pillPaddingBottom
+      const left = windowWidth - right
+      const {backgroundColor, zIndex} = this.data
+      this.setData({
+        pillRight: left,
+        pillwidth: width,
+        height: totalHeight,
+        navBarStyles: `height: ${totalHeight}px; padding-top: ${top}px;background-color: ${backgroundColor}; z-index: ${zIndex}`,
+        titleStyles: `height: ${height}px;`,
+        leftCornerStyles: `height: ${height}px; padding-left: ${left}px`
+      })
+    },
+    goBack() {
+      wx.navigateBack()
+    },
+    goHome() {
+      wx.reLaunch({url: this.data.homePath})
+    }
+  }
+})
